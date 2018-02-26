6dfa2fdad8aa6459586c31c70e55b3dd00b74059
Vlad Stupakov <stupakov41@gmail.com>
2018-02-26 22:26:40 +0200

    correcting commit

---
+++ Actions/ActionManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ActionManager {
+	
+	static string _actionType;
+
+	public static void ActionMaker(List<Human> selectedObjects, GameObject target, Vector3 position, bool isHeal)
+	{		
+		switch (target.tag) 
+		{
+			default:
+				_actionType = "move";
+				break;
+			case "Zombie":
+				_actionType = "attack";
+				break;
+			case "Chest":
+				_actionType = "open";
+				break;
+			case "Human":
+				if(isHeal)
+					_actionType = "heal";
+				else
+					_actionType = "move";
+			break;
+		}
+
+		foreach (Human h in selectedObjects) 
+		{
+			h.GetComponent<ActionMaker>().MakeAction(target, position, _actionType);
+		}
+	}
+}
