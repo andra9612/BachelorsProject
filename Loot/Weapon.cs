0a29015e2b75a0ad2fd48f8c48f7608a60f4265c
Vlad Stupakov <stupakov41@gmail.com>
2018-02-26 22:35:01 +0200

    corrective commit

---
+++ Loot/Weapon.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : Item {
+	private int _damage;
+	private int _range;
+
+	public int Damage{
+		get{	
+			return _damage;
+		}
+		set{ 
+			_damage = value;
+		}
+	}
+
+	public int Range{
+		get{	
+			return _range;
+		}
+		set{ 
+			_range = value;
+		}
+	}
+}
